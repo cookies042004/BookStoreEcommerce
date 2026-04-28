8e21c74f73025005f5a2452c00038dc3a2ce28ba
Akhil Puri <ap4723@srmist.edu.in>
2026-04-29 01:06:59 +0530

    [Akhil] Added: wishlist management microservice

---
+++ service/wishlist-service/src/main/java/com/bookstore/wishlist_service/repository/WishlistRepository.java
@@ -0,0 +1,11 @@
+package com.bookstore.wishlist_service.repository;
+
+import com.bookstore.wishlist_service.entity.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+
+    List<Wishlist> findByUserId(Long userId);
+}
