69423f4ed6454592828429f28b8d1e652c7daef0
Akhil Puri <ap4723@srmist.edu.in>
2026-04-29 10:54:27 +0530

    [Akhil] Added: Admin microservice

---
+++ service/admin-service/src/main/java/com/bookstore/admin_service/exception/AdminException.java
@@ -0,0 +1,7 @@
+package com.bookstore.admin_service.exception;
+
+public class AdminException extends RuntimeException {
+    public AdminException(String message) {
+        super(message);
+    }
+}
